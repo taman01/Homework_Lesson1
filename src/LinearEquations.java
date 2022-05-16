40ec5760f777fb8f909a9ed23939152688fa16a1
taman01 <tomaevgenij@yandex.ru>
2022-05-16 16:56:45 +0300

    Initial commit

---
+++ src/LinearEquations.java
@@ -0,0 +1,8 @@
+public class LinearEquations {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 15;
+        int x = -b/a;
+        System.out.println(x);
+    }
+}
---
+++ src/MathOperation.java
@@ -0,0 +1,21 @@
+public class MathOperation {
+    public static void main(String[] args) {
+        // Инициализируем переменные типа int
+        int a = 12;
+        int b = 5;
+        System.out.println("Сумма чисел = " + (a + b));
+        System.out.println("Разность чисел = " + (a - b));
+        System.out.println("Произведение чисел = " + (a * b));
+        System.out.println("Частное чисел = " + (a / b)); //
+        // Инициализируем переменные типа double
+        double c = 12.5;
+        double d = 5;
+        System.out.println("-------------------------------");
+        System.out.println("Сумма чисел = " + (c + d));
+        System.out.println("Разность чисел = " + (c - d));
+        System.out.println("Произведение чисел = " + (c * d));
+        System.out.println("Частное чисел = " + (c / d));
+
+
+    }
+}
